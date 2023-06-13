62cf65a132bd038ff63c3ca33d0a9110d1657b40
Emanuel Nedelcu <emanuel@mytime.com>
2023-06-13 19:48:12 +0300

    some fixes for ruby 3

--- lib/active_record/connection_adapters/redshift/schema_statements.rb
+++ lib/active_record/connection_adapters/redshift/schema_statements.rb
@@ -5,7 +5,7 @@ module ActiveRecord
         private
 
         def visit_ColumnDefinition(o)
-          o.sql_type = type_to_sql(o.type, o.options)
+          o.sql_type = type_to_sql(o.type, **o.options)
           super
         end
 
@@ -387,7 +387,7 @@ module ActiveRecord
         end
 
         # Maps logical Rails types to PostgreSQL-specific data types.
-        def type_to_sql(type, limit: nil, precision: nil, scale: nil, **)
+        def type_to_sql(type, limit: nil, precision: nil, scale: nil, **) # :nodoc:
           case type.to_s
           when 'integer'
             return 'integer' unless limit
